c5dc27ea27084bfc0dfd7078aa89db0b61918ce8
default <houkaidev@gmail.com>
2026-04-07 00:18:45 +0000

    fix: add GuildVoiceStates intent to client configuration

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildVoiceStates
     ]
 });
 
